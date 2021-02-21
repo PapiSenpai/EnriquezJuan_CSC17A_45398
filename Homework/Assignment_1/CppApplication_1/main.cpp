c2b124501068aa5629817c57b52d2dd60aa3d80a
Juan Enriquez <juan03192001@gmail.com>
2021-02-20 18:56:03 -0800

    Signed-off-by: Juan Enriquez <juan03192001@gmail.com>

---
+++ Homework/Assignment_1/CppApplication_1/main.cpp
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: Juan Enriquez
+ * Assignment: Hello World
+ * 
+ * Created on February 20, 2021, 6:25 PM
+ */
+
+#include <iostream>
+
+
+/*
+ * 
+ */
+int main() {
+
+    std::cout << "Hello World!";
+    
+    return 0;
+}
+
