0d540010e1c3692a74012f56ca100ebd3bf05d4a
Juan Enriquez <juan03192001@gmail.com>
2021-02-21 17:50:29 -0800

    Signed-off-by: Juan Enriquez <juan03192001@gmail.com>

--- Homework/Assignment_1/HelloWorld/nbproject/Makefile-variables.mk
+++ Homework/Assignment_1/HelloWorld/nbproject/Makefile-variables.mk
@@ -9,19 +9,19 @@ CND_DISTDIR=dist
 # Debug configuration
 CND_PLATFORM_Debug=Cygwin-Windows
 CND_ARTIFACT_DIR_Debug=dist/Debug/Cygwin-Windows
-CND_ARTIFACT_NAME_Debug=cppapplication_1
-CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/cppapplication_1
+CND_ARTIFACT_NAME_Debug=helloworld
+CND_ARTIFACT_PATH_Debug=dist/Debug/Cygwin-Windows/helloworld
 CND_PACKAGE_DIR_Debug=dist/Debug/Cygwin-Windows/package
-CND_PACKAGE_NAME_Debug=cppapplication1.tar
-CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/cppapplication1.tar
+CND_PACKAGE_NAME_Debug=helloworld.tar
+CND_PACKAGE_PATH_Debug=dist/Debug/Cygwin-Windows/package/helloworld.tar
 # Release configuration
 CND_PLATFORM_Release=Cygwin-Windows
 CND_ARTIFACT_DIR_Release=dist/Release/Cygwin-Windows
-CND_ARTIFACT_NAME_Release=cppapplication_1
-CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/cppapplication_1
+CND_ARTIFACT_NAME_Release=helloworld
+CND_ARTIFACT_PATH_Release=dist/Release/Cygwin-Windows/helloworld
 CND_PACKAGE_DIR_Release=dist/Release/Cygwin-Windows/package
-CND_PACKAGE_NAME_Release=cppapplication1.tar
-CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/cppapplication1.tar
+CND_PACKAGE_NAME_Release=helloworld.tar
+CND_PACKAGE_PATH_Release=dist/Release/Cygwin-Windows/package/helloworld.tar
 #
 # include compiler specific variables
 #
--- Homework/Assignment_1/HelloWorld/nbproject/Package-Release.bash
+++ Homework/Assignment_1/HelloWorld/nbproject/Package-Release.bash
@@ -13,9 +13,9 @@ CND_BUILDDIR=build
 CND_DLIB_EXT=dll
 NBTMPDIR=${CND_BUILDDIR}/${CND_CONF}/${CND_PLATFORM}/tmp-packaging
 TMPDIRNAME=tmp-packaging
-OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/cppapplication_1
-OUTPUT_BASENAME=cppapplication_1
-PACKAGE_TOP_DIR=cppapplication1/
+OUTPUT_PATH=${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/helloworld
+OUTPUT_BASENAME=helloworld
+PACKAGE_TOP_DIR=helloworld/
 
 # Functions
 function checkReturnCode
@@ -60,15 +60,15 @@ mkdir -p ${NBTMPDIR}
 
 # Copy files and create directories and links
 cd "${TOP}"
-makeDirectory "${NBTMPDIR}/cppapplication1/bin"
+makeDirectory "${NBTMPDIR}/helloworld/bin"
 copyFileToTmpDir "${OUTPUT_PATH}.exe" "${NBTMPDIR}/${PACKAGE_TOP_DIR}bin/${OUTPUT_BASENAME}.exe" 0755
 
 
 # Generate tar file
 cd "${TOP}"
-rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/cppapplication1.tar
+rm -f ${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/helloworld.tar
 cd ${NBTMPDIR}
-tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/cppapplication1.tar *
+tar -vcf ../../../../${CND_DISTDIR}/${CND_CONF}/${CND_PLATFORM}/package/helloworld.tar *
 checkReturnCode
 
 # Cleanup
